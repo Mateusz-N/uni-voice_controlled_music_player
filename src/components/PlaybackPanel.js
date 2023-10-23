d30c28291d5b58ac981526c67c7de053bfd31014
Mateusz-N <nosel.mateusz@gmail.com>
2023-10-23 20:08:32 +0200

    Zaimplementuj podstawową logikę odtwarzania

--- src/components/PlaybackPanel.js
+++ src/components/PlaybackPanel.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Styles from './PlaybackPanel.module.scss'
 
@@ -17,15 +17,68 @@ const millisecondsToFormattedTime = (milliseconds) => {
     return addLeadingZero(minutes) + ':' + addLeadingZero(seconds);
 }
 
+const resetPlayback = (handleUpdateTimeElapsed, handleTogglePause) => {
+    handleUpdateTimeElapsed(0);
+    handleTogglePause();
+}
+
+const updatePlayback = (trackDuration_ms, timeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause) => {
+    if(trackDuration_ms - timeElapsed_ms >= 10) {
+        handleUpdateTimer(setTimeout(() => {
+            handleUpdateTimeElapsed(timeElapsed_ms + 10);
+        }, 10)); // odstępy niższe niż 10 mogą nie działać poprawnie ze względu na ograniczenia przeglądarki
+    }
+    else {
+        resetPlayback(handleUpdateTimeElapsed, handleTogglePause)
+    }
+}
+
+const togglePlayback = (paused, trackDuration_ms, getTimeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause) => {
+    if(paused) {
+        handleUpdateTimer(-1);
+    }
+    else {
+        updatePlayback(trackDuration_ms, getTimeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause);
+    }
+}
+
 const PlaybackPanel = () => {
+    let trackDuration_ms = 15000;
+
+    const [timer, setTimer] = useState(null);
+    const [timeElapsed_ms, setTimeElapsed] = useState(0);
     const [paused, setPaused] = useState(true);
+    
+    const handleUpdateTimer = (newTimer) => {
+        // console.log(timer)
+        if(newTimer === -1) {
+            clearTimeout(timer)
+        }
+        else {
+            setTimer(newTimer);
+        }
+    }
+    const handleUpdateTimeElapsed = (newTime) => {
+        setTimeElapsed(newTime);
+    }
+    useEffect(() => {
+        if(!paused) {
+            updatePlayback(trackDuration_ms, timeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [timeElapsed_ms])
     const handleTogglePause = () => {
         setPaused(prevState => !prevState);
     }
-    let timeElapsed_ms = 1500;
-    let trackDuration_ms = 15000
+
+    /* Oczekuje zakończenia wykonywania setPaused() */
+    useEffect(() => {
+        togglePlayback(paused, trackDuration_ms, timeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [paused]);
+
     let timeElapsed_formatted = millisecondsToFormattedTime(timeElapsed_ms);
-    let trackDuration_formatted = millisecondsToFormattedTime(trackDuration_ms);;
+    let trackDuration_formatted = millisecondsToFormattedTime(trackDuration_ms);
     
     return(
         <div id = {Styles.playbackPanel}>
@@ -37,7 +90,7 @@ const PlaybackPanel = () => {
             <div id = {Styles.progressSection}>
                 <p id = {Styles.timeElapsed}>{timeElapsed_formatted}</p>
                 <div id = {Styles.progressBar}>
-                    <div id = {Styles.progressBarFill} style = {{width: (timeElapsed_ms / trackDuration_ms * 100) + '%;'}}>
+                    <div id = {Styles.progressBarFill} style = {{width: (timeElapsed_ms / trackDuration_ms * 100) + '%'}}>
 
                     </div>
                 </div>

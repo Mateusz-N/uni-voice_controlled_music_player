80bc2300d05026aec755237118865025728f3537
Mateusz-N <nosel.mateusz@gmail.com>
2023-10-27 18:26:58 +0200

    Dodaj interaktywne manipulowanie paskiem odtwarzania

--- src/common/auxiliaryFunctions.js
+++ src/common/auxiliaryFunctions.js
@@ -13,27 +13,27 @@ export const millisecondsToFormattedTime = (milliseconds) => {
     return addLeadingZero(minutes) + ':' + addLeadingZero(seconds);
 }
 
-export const resetPlayback = (handleUpdateTimeElapsed, handleTogglePause) => {
-    handleUpdateTimeElapsed(0);
+export const resetPlayback = (setCurrentTimestamp, handleTogglePause) => {
+    setCurrentTimestamp(0);
     handleTogglePause();
 }
 
-export const updatePlayback = (trackDuration_ms, timeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause) => {
-    if(trackDuration_ms - timeElapsed_ms >= 10) {
+export const updatePlayback = (trackDuration_ms, currentTimestamp_ms, setCurrentTimestamp, handleUpdateTimer, handleTogglePause) => {
+    if(trackDuration_ms - currentTimestamp_ms >= 10) {
         handleUpdateTimer(setTimeout(() => {
-            handleUpdateTimeElapsed(timeElapsed_ms + 10);
+            setCurrentTimestamp(currentTimestamp_ms + 10);
         }, 10)); // odstępy niższe niż 10 mogą nie działać poprawnie ze względu na ograniczenia przeglądarki
     }
     else {
-        resetPlayback(handleUpdateTimeElapsed, handleTogglePause)
+        resetPlayback(setCurrentTimestamp, handleTogglePause)
     }
 }
 
-export const togglePlayback = (paused, trackDuration_ms, getTimeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause) => {
+export const togglePlayback = (paused, trackDuration_ms, getCurrentTimestamp_ms, setCurrentTimestamp, handleUpdateTimer, handleTogglePause) => {
     if(paused) {
         handleUpdateTimer(-1);
     }
     else {
-        updatePlayback(trackDuration_ms, getTimeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause);
+        updatePlayback(trackDuration_ms, getCurrentTimestamp_ms, setCurrentTimestamp, handleUpdateTimer, handleTogglePause);
     }
 }
--- src/components/PlaybackPanel.js
+++ src/components/PlaybackPanel.js
@@ -12,11 +12,14 @@ const PlaybackPanel = () => {
     let trackDuration_ms = 15000;
 
     const [timer, setTimer] = useState(null);
-    const [timeElapsed_ms, setTimeElapsed] = useState(0);
+    const [currentTimestamp_ms, setCurrentTimestamp] = useState(0);
+    const [targetTimestamp_ms, setTargetTimestamp] = useState(0);   // Docelowy punkt w czasie w przypadku przeciągania paska postępu
+                                                                    // (użytkownik może dowolnie pasek przesuwać, w międzyczasie muzyka nadal gra zgodnie z domyślnym biegiem)
     const [paused, setPaused] = useState(true);
+    const [progressBarPreviewWidth, setProgressBarPreviewWidth] = useState(0);
+    const [progressBarInDragMode, setProgressBarInDragMode] = useState(false);
     
     const handleUpdateTimer = (newTimer) => {
-        // console.log(timer)
         if(newTimer === -1) {
             clearTimeout(timer)
         }
@@ -24,8 +27,8 @@ const PlaybackPanel = () => {
             setTimer(newTimer);
         }
     }
-    const handleUpdateTimeElapsed = (newTime) => {
-        setTimeElapsed(newTime);
+    const handleTogglePause = () => {
+        setPaused(prevState => !prevState);
     }
     const handlePreviousTrack = () => {
         // to-do
@@ -33,23 +36,69 @@ const PlaybackPanel = () => {
     const handleNextTrack = () => {
         // to-do
     }
+    const getRelativeProgressBarPointPosition = (event) => {
+        const progressBarBoundingRect = document.getElementById(Styles.progressBar).getBoundingClientRect(); // currentTarget przechwytuje źródłowy element (progressBar) zamiast wierzchniego (progressBarFill)
+        return (event.clientX - progressBarBoundingRect.left) / progressBarBoundingRect.width; // Część całości paska na którą wskazuje zdarzenie [0, 1]
+    }
+    const handleProgressBarMouseDown = (event) => {
+        if(event.button === 0) { // Tylko lewy przycisk myszy
+            setProgressBarInDragMode(true);
+            clearTimeout(timer);
+            const newTime = getRelativeProgressBarPointPosition(event) * trackDuration_ms;
+            setTargetTimestamp(newTime);
+        }
+    }
+    const handleProgressBarHover = (event) => {
+        if(event.type === 'mouseout') {
+            setProgressBarPreviewWidth(0);
+        }
+        else {
+            const progressBarPercentageHovered = getRelativeProgressBarPointPosition(event) * 100;
+            setProgressBarPreviewWidth(progressBarPercentageHovered);
+        }
+    }
+    const handleProgressBarDrag = (event) => {
+        if(progressBarInDragMode) {
+            clearTimeout(timer);
+            const newTime = getRelativeProgressBarPointPosition(event) * trackDuration_ms;
+            setTargetTimestamp(newTime);
+        }
+    }
+    const handleProgressBarMouseUp = () => {
+        if(progressBarInDragMode) {
+            setProgressBarInDragMode(false);
+            // setCurrentTimestamp(targetTimestamp_ms);
+            document.removeEventListener('mousemove', handleProgressBarDrag);
+            document.removeEventListener('mouseup', handleProgressBarMouseUp);
+        }
+    }
 
     useEffect(() => {
         if(!paused) {
-            updatePlayback(trackDuration_ms, timeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause);
+            updatePlayback(trackDuration_ms, currentTimestamp_ms, setCurrentTimestamp, handleUpdateTimer, handleTogglePause);
         }
+        setTargetTimestamp(currentTimestamp_ms);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [timeElapsed_ms])
-    const handleTogglePause = () => {
-        setPaused(prevState => !prevState);
-    }
-    /* Oczekuje zakończenia wykonywania setPaused() */
+    }, [currentTimestamp_ms])
     useEffect(() => {
-        togglePlayback(paused, trackDuration_ms, timeElapsed_ms, handleUpdateTimeElapsed, handleUpdateTimer, handleTogglePause);
+        togglePlayback(paused, trackDuration_ms, currentTimestamp_ms, setCurrentTimestamp, handleUpdateTimer, handleTogglePause);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [paused]);
+    useEffect(() => {
+        if(progressBarInDragMode) {
+            document.addEventListener('mousemove', handleProgressBarDrag);
+            document.addEventListener('mouseup', handleProgressBarMouseUp);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [progressBarInDragMode]);
+    useEffect(() => {
+        if(!progressBarInDragMode) {
+            setCurrentTimestamp(targetTimestamp_ms);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [targetTimestamp_ms])
 
-    let timeElapsed_formatted = millisecondsToFormattedTime(timeElapsed_ms);
+    let currentTimestamp_formatted = millisecondsToFormattedTime(currentTimestamp_ms);
     let trackDuration_formatted = millisecondsToFormattedTime(trackDuration_ms);
     
     return(
@@ -60,9 +109,18 @@ const PlaybackPanel = () => {
                 <img src = {btn_next} alt = 'Next track' id = {paused ? Styles.btnPlay : Styles.btnPause} className = {Styles.btnTogglePlayback} onClick = {handleNextTrack} />
             </div>
             <div id = {Styles.progressSection}>
-                <p id = {Styles.timeElapsed}>{timeElapsed_formatted}</p>
-                <div id = {Styles.progressBar}>
-                    <div id = {Styles.progressBarFill} style = {{width: (timeElapsed_ms / trackDuration_ms * 100) + '%'}}>
+                <p id = {Styles.timeElapsed}>{currentTimestamp_formatted}</p>
+                <div
+                    id = {Styles.progressBar}
+                    onMouseDown = {event => handleProgressBarMouseDown(event)}
+                    onMouseOver = {event => handleProgressBarHover(event)}
+                    onMouseOut = {event => handleProgressBarHover(event)}
+                    onMouseMove = {event => handleProgressBarHover(event)}
+                >
+                    <div id = {Styles.progressBarPreview} style = {{width: progressBarPreviewWidth + '%'}}>
+                        
+                    </div>
+                    <div id = {Styles.progressBarFill} style = {{width: (targetTimestamp_ms / trackDuration_ms * 100) + '%'}}>
 
                     </div>
                 </div>

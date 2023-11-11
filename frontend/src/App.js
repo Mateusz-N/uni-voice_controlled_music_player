7157890ee7dfac3d397cf035e5c5f2e04131bf6d
Mateusz-N <nosel.mateusz@gmail.com>
2023-11-11 11:04:28 +0100

    Utwórz szablon komponentu catalogBrowser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import PlaybackPanel from './components/PlaybackPanel';
-import VoiceInputButton from './components/VoiceInputButton';
+import CatalogBrowser from './components/CatalogBrowser';
 import placeholderAlbumCoverSrc from './resources/albumCover_placeholder.png';
 import './App.css';
 
 const App = () => {
   return (
     <div id = 'page'>
-      <div id = 'content'>
-        <VoiceInputButton></VoiceInputButton>
-      </div>
+      <CatalogBrowser>
+        
+      </CatalogBrowser>
       <PlaybackPanel track =
         {{
           duration_ms: '15000',
---
+++ frontend/src/components/CatalogBrowser.js
@@ -0,0 +1,11 @@
+import Styles from './CatalogBrowser.module.scss';
+
+const CatalogBrowser = () => {
+    return(
+        <div id = {Styles.catalogBrowser}>
+
+        </div>
+    );
+}
+
+export default CatalogBrowser;

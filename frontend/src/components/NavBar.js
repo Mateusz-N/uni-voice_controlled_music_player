86f845d45c6d7535ce207c845dab4aad48975f74
Mateusz-N <nosel.mateusz@gmail.com>
2023-11-11 19:11:09 +0100

    Nadaj właściwy wygląd i strukturę komponentowi searchBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,13 @@
+import SearchBar from './SearchBar';
+
 import Styles from './NavBar.module.scss';
 
 const NavBar = () => {
     return(
         <div id = {Styles.navBar}>
-
+            <SearchBar>
+                
+            </SearchBar>
         </div>
     );
 }
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,9 +1,11 @@
-import Styles from 'SearchBar.module.scss';
+import icon_search from '../resources/icon_search.svg';
+import Styles from './SearchBar.module.scss';
 
 const SearchBar = () => {
     return(
         <div id = {Styles.searchBar}>
-
+            <input id = {Styles.searchInput} placeholder = 'Search for track, artist, album or playlist...' />
+            <img id = {Styles.searchIcon} src = {icon_search} alt = 'Search' />
         </div>
     );
 }

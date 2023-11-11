ff91a433a1c11f08ac02eb38b29eb27f74dd9de3
Mateusz-N <nosel.mateusz@gmail.com>
2023-11-11 11:29:09 +0100

    Utwórz szablon komponentu searchBar

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import Styles from 'SearchBar.module.scss';
+
+const SearchBar = () => {
+    return(
+        <div id = {Styles.searchBar}>
+
+        </div>
+    );
+}
+
+export default SearchBar;
